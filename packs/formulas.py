402a38ea9d3a7d81f5b0a9b5f07b6a4ef311e2dd
BenniFSST <bmangott@tsn.at>
2022-03-17 21:55:13 +0100

    Englisch Verbesserung

--- packs/formulas.py
+++ packs/formulas.py
@@ -10,7 +10,7 @@ def calculation():
         widgets.destroy()
     
     # ------------------------------------------- Text
-    nt1 = Label(rechner, text="On the following page are the formulas and explenations on how to calculate all\n the Highpasses, Lowpasses and resistor circuits yourself!", bg="white",font=("Calibri", 12, "bold"), justify = LEFT)
+    nt1 = Label(rechner, text="On the following page are the formulas and explenations on how to calculate all\n the highpasses, lowpasses and resistor circuits yourself!", bg="white",font=("Calibri", 12, "bold"), justify = LEFT)
     nt1.place(x=25, y=5)
 
     #nt2 = Label(rechner, text="test")
@@ -19,19 +19,19 @@ def calculation():
     res1 = Label(rechner, text="Resistor serial and parallel: ", bg = "white", font=("Calibri", 10, "bold"))
     res1.place(x=40, y=60)
 
-    calc1 = Label(rechner, text="The resistor calculations are simple and can mostly be done by hand!\n For serial Resistors you only add up there resistor values [Ohm].\n To calculate TWO parallel resistors you can use the following formula: ", bg = "white", justify = LEFT)
+    calc1 = Label(rechner, text="The resistor calculations are simple and can mostly be done by hand!\n For serial resistors you only add up there resistor values [Ohm].\n To calculate TWO parallel resistors you can use the following formula: ", bg = "white", justify = LEFT)
     calc1.place(x=40, y=90)
 
     rc2 = Label(rechner, text="RC highpass and lowpass: ", bg="white", font=("Calibri", 10, "bold"))
     rc2.place(x=40, y=240)
 
-    calc2 = Label(rechner, text="The RC passes frequencys are a little bit more complicated to calculate.\n (might require a calculator) Regardless of whether you want to calculate\n the high or lowpass you will need this fomrula for both: ", bg="white", justify = LEFT)
+    calc2 = Label(rechner, text="The RC passes frequencys are a little bit more complicated to calculate.\n (might require a calculator) Regardless of whether you want to calculate\n the high or lowpass you will need this formula for both: ", bg="white", justify = LEFT)
     calc2.place(x=40, y=270)
 
     rl3 = Label(rechner, text="RC highpass and lowpass: ", bg="white", font=("Calibri", 10, "bold"))
     rl3.place(x=40, y=420)
 
-    calc3 = Label(rechner, text="The RL passes frequencys calculations are very similar to the ones above.\n (might require a calculator) The formula is again the same for both\n high and lowpass, as follows below: ", bg="white", justify = LEFT)
+    calc3 = Label(rechner, text="The RL passes frequency calculations are very similar to the ones above.\n (might require a calculator) The formula is again the same for both\n high and lowpass as follows below: ", bg="white", justify = LEFT)
     calc3.place(x=40, y=450)
 
     sdnt = Label(rechner, text="Note: In case a bandpass filter (mix of a high and lowpass filter) has to\n be used or calculated, we recommend to google for a solution. For example\n Electronicbase.net can be usefull here!", bg="white", justify = LEFT)
